23a17adaefa480f35291669f1438a729a12d4f0e
JATIN SAREEN <102468240+jatin-12-2002@users.noreply.github.com>
2024-10-27 17:57:36 +0000

    model evaluation pipeline done

--- hate/entity/artifact_entity.py
+++ hate/entity/artifact_entity.py
@@ -17,3 +17,8 @@ class ModelTrainerArtifacts:
     trained_model_path:str
     x_test_path: list
     y_test_path: list
+
+
+@dataclass
+class ModelEvaluationArtifacts:
+    is_model_accepted: bool 
